74ce445baed1f23f89dbf4a09d9b5f96e04da39d
François Kooman <fkooman@tuxed.net>
2017-03-21 23:44:12 +0100

    add simple filter search

---
+++ web/js/discovery.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var options = {
+        valueNames: [ 'name' ]
+    };
+
+    var entityList = new List('entity-list', options);
+});
